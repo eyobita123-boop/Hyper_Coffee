1469f81648496079b798dc92760ff3d43ac87118
Eyob Dereje Bekele <eyobita123@gmail.com>
2026-06-20 20:03:37 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'hyper-coffee-v1';
+const ASSETS = [
+  'index.html',
+  'order.html',
+  'journal.html',
+  'events.html',
+  'assets/style.css',
+  'assets/site.js',
+  'https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,400..700&family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500;600&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
